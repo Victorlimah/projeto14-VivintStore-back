442b9866b8422c13a6409a83d54a93a000c62801
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-05-11 18:49:14 -0300

    bug fixed: now all responses return an object

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,72 +1,75 @@
-import db from '../db.js';
-import bcrypt from 'bcrypt';
-import joi from 'joi';
-import jwt from 'jsonwebtoken';
-import { stripHtml } from 'string-strip-html';
+import db from "../db.js";
+import bcrypt from "bcrypt";
+import joi from "joi";
+import jwt from "jsonwebtoken";
+import { stripHtml } from "string-strip-html";
 
 export async function signUp(req, res) {
-    const { name, email, password, confirmationPassword } = req.body;
-    const emailVerification = await db.collection('users').findOne({ email });
-    const userId = Date.now();
-    const sazitizedName = stripHtml(name).result.trim();
-    const schema = joi.object({
-        name: joi.string().required(),
-        email: joi.string().email().required(),
-        password: joi.string().required(),
-        confirmationPassword: joi.string().required()
+  const { name, email, password, confirmationPassword } = req.body;
+  const emailVerification = await db.collection("users").findOne({ email });
+  const userId = Date.now();
+  const sazitizedName = stripHtml(name).result.trim();
+  const schema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmationPassword: joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) {
+    res.status(422).send(error.details.map((d) => d.message));
+    return;
+  }
+  if (emailVerification) {
+    return res.status(409).send({ message: "O email já está cadastrado" });
+  }
+  if (password !== confirmationPassword) {
+    return res.status(401).send({ message: "As senhas não conferem" });
+  }
+  try {
+    await db.collection("users").insertOne({
+      sazitizedName,
+      email,
+      password: await bcrypt.hash(password, 10),
+      userId,
     });
-
-
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-        res.status(422).send(error.details.map(d => d.message));
-        return;
-    }
-    if (emailVerification) {
-        return res.status(409).send("O email já está cadastrado");
-    }
-    if (password !== confirmationPassword) {
-        return res.status(401).send({ message: "As senhas não conferem" });
-    }
-    try {
-        await db.collection('users').insertOne({ sazitizedName, email, password: await bcrypt.hash(password, 10), userId });
-        res.status(201).json({ message: 'Usuario cadastrado' });
-    } catch {
-        res.status(500).send({ message: 'Erro ao cadastrar usuario' });
-    }
+    res.status(201).json({ message: "Usuário cadastrado" });
+  } catch {
+    res.status(500).send({ message: "Erro ao cadastrar usuário" });
+  }
 }
 
 export async function signIn(req, res) {
-    const { email, password } = req.body;
-    const sectionId = Date.now();
-    const chaveSecreta = process.env.JWT_SECRET;
+  const { email, password } = req.body;
+  const sectionId = Date.now();
+  const chaveSecreta = process.env.JWT_SECRET;
 
-    const schema = joi.object({
-        email: joi.string().email().required(),
-        password: joi.string().required()
-    });
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
 
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-        res.status(422).send(error.details.map(d => d.message));
-        return;
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) {
+    res.status(422).send(error.details.map((d) => d.message));
+    return;
+  }
+
+  try {
+    const user = await db.collection("users").findOne({ email });
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (user && isValid) {
+      const dados = { userId: user.userId, sectionId };
+      const token = jwt.sign(dados, chaveSecreta, { expiresIn: "1h" });
+
+      await db.collection("sections").insertOne({ token, user: user.email });
+      res.status(200).json({ token, user: user.name });
+    } else {
+      res.status(401).send({ message: "Usuario ou senha incorretos" });
     }
-
-    try {
-        const user = await db.collection("users").findOne({ email });
-        const isValid = await bcrypt.compare(password, user.password);
-
-        if (user && isValid) {
-            const dados = { userId: user.userId, sectionId };
-            const token = jwt.sign(dados, chaveSecreta, { expiresIn: '1h' });
-
-            await db.collection("sections").insertOne({ token, user: user.email });
-            res.status(200).json({ token, user: user.name });
-        }
-        else {
-            res.status(401).send('Usuario ou senha incorretos');
-        }
-    } catch {
-        res.status(500).send('Erro ao logar');
-    }
+  } catch {
+    res.status(500).send({ message: "Erro ao logar" });
+  }
 }
